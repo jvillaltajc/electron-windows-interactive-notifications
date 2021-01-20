38a26b1e4c4c8ee23d80bb28841f9bf7b03c5bee
Juan Carlos Villalta <60619047+jvillaltajc@users.noreply.github.com>
2021-01-20 17:33:41 -0600

    Update postinstall.js
    rollback of last change

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const releaseKey = 'Release'
+const releaseKey = process.arch === 'x64' ? 'x64/Release' : 'Release'
 const from = path.join(__dirname, '..', releaseKey, 'InteractiveNotifications.dll')
 const to = path.join(__dirname, '..', 'build', 'Release', 'InteractiveNotifications.dll')
 
