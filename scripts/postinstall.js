b81c086add29d7cd83b05612b76d57d0e21b3398
Juan Carlos Villalta <60619047+jvillaltajc@users.noreply.github.com>
2021-01-20 16:20:12 -0600

    Update postinstall.js
    only build a 32bit regardless of current machine architecture

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const releaseKey = process.arch === 'x64' ? 'x64/Release' : 'Release'
+const releaseKey = 'Release'
 const from = path.join(__dirname, '..', releaseKey, 'InteractiveNotifications.dll')
 const to = path.join(__dirname, '..', 'build', 'Release', 'InteractiveNotifications.dll')
 
